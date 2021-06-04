cbbec1090d2dceb6d229695a316eeff38df55025
AD9018 <adamdadi23@gmail.com>
2021-06-03 22:58:47 -0400

    WIP

--- back-end/server.js
+++ back-end/server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const helmet = require('helmet')
 const cors = require('cors');
-
+const path = require('path')
 // const UsersRouter = require('./api/users/users-router')
+
+
 
 
 const server = express()
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
+server.use(express.static(path.join(__dirname, '../front-end/build')))
 
 
 // server.use("/api/users",UsersRouter)
