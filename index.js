75b7cca6ab8484ab4f766d575cd3f99d9bf78848
AD9018 <adamdadi23@gmail.com>
2021-06-03 20:07:52 -0400

    WIP

---
+++ index.js
@@ -0,0 +1,15 @@
+require("dotenv").config()
+const server = require('./server');
+
+
+
+
+const PORT = process.env.PORT || 4000
+
+
+
+
+
+server.listen(PORT, () => {
+    console.log(`\n*** Server Running on http://localhost:4000 ***\n`)
+})
--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 const express = require('express');
+const helmet = require('helmet')
+const cors = require('cors');
+
+// const UsersRouter = require('./api/users/users-router')
+
+
+const server = express()
+server.use(express.json())
+server.use(helmet())
+server.use(cors())
+
+
+// server.use("/api/users",UsersRouter)
+
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({ 
+        message:err.message,
+        status:err.stack,
+    })
+})
+
+module.exports = server
